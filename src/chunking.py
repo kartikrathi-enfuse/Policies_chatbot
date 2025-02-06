58245f61e3caf85c61bacea4297f96fbd285aded
kartikrathi-enfuse <kartik.rathi@enfuse-solutions.com>
2025-02-06 17:06:10 +0530

    Chunk_size and Overlap value increased

--- src/chunking.py
+++ src/chunking.py
@@ -1,6 +1,6 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-def split_text_into_chunks(text, chunk_size=700, chunk_overlap=75):
+def split_text_into_chunks(text, chunk_size=512, chunk_overlap=55):
     """Split text into smaller chunks."""
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
@@ -8,7 +8,7 @@ def split_text_into_chunks(text, chunk_size=700, chunk_overlap=75):
     )
     return text_splitter.split_text(text)
 
-def chunk_text_from_all_pdfs(pdf_texts, chunk_size=700, chunk_overlap=75):
+def chunk_text_from_all_pdfs(pdf_texts, chunk_size=512, chunk_overlap=55):
     """Chunk text from all PDF files."""
     all_chunks = []
     for pdf_text in pdf_texts:
