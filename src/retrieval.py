aaa44c1ff62e4b3a3560e02d2564d8f975a6ab1d
kartikrathi-enfuse <kartik.rathi@enfuse-solutions.com>
2025-03-06 14:13:15 +0530

    Threshold removed

--- src/retrieval.py
+++ src/retrieval.py
@@ -2,26 +2,27 @@ from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
 
-def retrieve_relevant_chunks(query, index, metadata, top_k, model_name='sentence-transformers/paraphrase-MiniLM-L6-v2',threshold=0.3):
+def retrieve_relevant_chunks(query, index, metadata, top_k, model_name='sentence-transformers/paraphrase-MiniLM-L6-v2',threshold=0.1):
     """Retrieve top_k relevant chunks from the FAISS index using cosine similarity"""
     embedding_model = SentenceTransformer(model_name)
     query_embedding = embedding_model.encode([query]).astype(np.float32)
     faiss.normalize_L2(query_embedding) #Normalize to unit length
 
     distances, indices = index.search(query_embedding, top_k)
-    results = []
-    for idx, dist in zip(indices[0], distances[0]):
-        if idx != -1 and dist >= threshold:  # idx -1 represent invalid result
-            results.append((metadata[idx], dist))
+    results = [(metadata[idx], dist) for idx, dist in zip(indices[0], distances[0]) if idx != -1]
+    # results = []
+    # for idx, dist in zip(indices[0], distances[0]):
+    #     if idx != -1 and dist >= threshold:  # idx -1 represent invalid result
+    #         results.append((metadata[idx], dist))
 
     return results
 
 
 if __name__ == '__main__':
     from embedding import load_faiss_index, load_metadata
-    query = 'Will external vendors also give feedback at appraisal time'
+    query = 'Who is eligible for Internal Job Posting'
     faiss_index_path = r"D:\RAG\embeddings\hr_policy_faiss.index"
     metadata_path = r"D:\RAG\embeddings\hr_policy_faiss_metadata.pkl"
     index = load_faiss_index(faiss_index_path)
     metadata = load_metadata(metadata_path)
-    print(retrieve_relevant_chunks(query, index, metadata, top_k=4, model_name='all-MiniLM-L6-v2'))
+    print(retrieve_relevant_chunks(query, index, metadata, top_k=10, model_name='sentence-transformers/paraphrase-MiniLM-L6-v2'))
