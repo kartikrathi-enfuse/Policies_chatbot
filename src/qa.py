dfe3df6b9c5992f5bf2398fe22c2f915c0b6115a
kartikrathi-enfuse <kartik.rathi@enfuse-solutions.com>
2025-03-06 14:10:46 +0530

    Mistral model replaced by Google Gemini

--- src/qa.py
+++ src/qa.py
@@ -2,24 +2,23 @@ import os
 from langchain_huggingface import HuggingFaceEndpoint
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 def create_llm_chain(context, re_query, model_repo_id="mistralai/Mistral-7B-Instruct-v0.3"):
-    api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+    api_key = os.getenv("GOOGLE_API_KEY")
 
-    hf_model = HuggingFaceEndpoint(
-        repo_id=model_repo_id,
-        task="text-generation"
-        )
+    llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash', google_api_key=api_key,temperature=0.5)
 
     prompt_template = """
-    You are an AI assistant trained to answer questions about HR policies in a friendly and personalized tone.
-    Your responses should:
-    1. Be concise, Short and use conversational language.
-    2. Address the user directly (e.g., "You are entitled to..." instead of "Employees are entitled to...").
-    3. Use only the provided context to answer the query.
-    4. If the context does not include the information, respond with: "I'm sorry, but I don't have that information."
-    5. Avoid Phrases like "In the provided context" or "Based on Given context".
-    6.  Don't assume, Dont calculate, dont infer and dont generate any new information.
+    You are an AI assistant trained to answer Enfuse Solutions Employees questions about HR policies in a friendly and personalized tone.
+    **IMPORTANT RULES **
+    - You **MUST** only use the provided context to answer the query.
+    - If the answer is **not explicitly mentioned in the context **, say :
+    ** I'm sorry, but I don't have that information.Please mail your query to hr@enfusesolutions.com for assistance.**
+    - **Don't infer, assume, or generate new information **
+    - Be concise, direct and Conversational.
+    - Address the user directly (e.g., "You are entitled to..." instead of "Employees are entitled to...").
+    - You must give answer from the provided context only.
 
     Context:
     {context}
@@ -35,9 +34,45 @@ def create_llm_chain(context, re_query, model_repo_id="mistralai/Mistral-7B-Inst
         template=prompt_template
     )
 
-    chain = prompt | hf_model | StrOutputParser()
+    chain = prompt | llm | StrOutputParser()
 
     return chain.invoke({'context': context, 'query': re_query})
+
+# def create_llm_chain(context, re_query, model_repo_id="mistralai/Mistral-7B-Instruct-v0.3"):
+#     api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+#     hf_model = HuggingFaceEndpoint(
+#         repo_id=model_repo_id,
+#         task="text-generation"
+#         )
+
+#     prompt_template = """
+#     You are an AI assistant trained to answer Enfuse Solutions Employees questions about HR policies in a friendly and personalized tone.
+#     Your responses should:
+#     1. Be concise, Short and use conversational language.
+#     2. Address the user directly (e.g., "You are entitled to..." instead of "Employees are entitled to...").
+#     3. Use only the provided context to answer the query.
+#     4. If the context does not include the information, respond with: "I'm sorry, but I don't have that information.".
+#     5. Avoid Phrases like "In the provided context" or "Based on Given context".
+#     6. Don't assume, Dont calculate, dont infer and dont generate any new information.
+
+#     Context:
+#     {context}
+
+#     Query:
+#     {query}
+
+#     Personalized Answer:
+#     """
+
+#     prompt = PromptTemplate(
+#         input_variables=["context", "re_query"],
+#         template=prompt_template
+#     )
+
+#     chain = prompt | hf_model | StrOutputParser()
+
+#     return chain.invoke({'context': context, 'query': re_query})
 
 
 if __name__ == '__main__':
