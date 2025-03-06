ab714fd9126a2c6893643330e1c924ffc3abb526
kartikrathi-enfuse <kartik.rathi@enfuse-solutions.com>
2025-03-06 14:14:05 +0530

    Tuned prompt templete

--- src/qa.py
+++ src/qa.py
@@ -38,42 +38,6 @@ def create_llm_chain(context, re_query, model_repo_id="mistralai/Mistral-7B-Inst
 
     return chain.invoke({'context': context, 'query': re_query})
 
-# def create_llm_chain(context, re_query, model_repo_id="mistralai/Mistral-7B-Instruct-v0.3"):
-#     api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-
-#     hf_model = HuggingFaceEndpoint(
-#         repo_id=model_repo_id,
-#         task="text-generation"
-#         )
-
-#     prompt_template = """
-#     You are an AI assistant trained to answer Enfuse Solutions Employees questions about HR policies in a friendly and personalized tone.
-#     Your responses should:
-#     1. Be concise, Short and use conversational language.
-#     2. Address the user directly (e.g., "You are entitled to..." instead of "Employees are entitled to...").
-#     3. Use only the provided context to answer the query.
-#     4. If the context does not include the information, respond with: "I'm sorry, but I don't have that information.".
-#     5. Avoid Phrases like "In the provided context" or "Based on Given context".
-#     6. Don't assume, Dont calculate, dont infer and dont generate any new information.
-
-#     Context:
-#     {context}
-
-#     Query:
-#     {query}
-
-#     Personalized Answer:
-#     """
-
-#     prompt = PromptTemplate(
-#         input_variables=["context", "re_query"],
-#         template=prompt_template
-#     )
-
-#     chain = prompt | hf_model | StrOutputParser()
-
-#     return chain.invoke({'context': context, 'query': re_query})
-
 
 if __name__ == '__main__':
     context = '''If an employee calls out of work the day before, the day of, or the day after a holiday, the 
